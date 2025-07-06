abf307d5ea37b3271fd3c6cf9224d736f69b8eb1
tanujasharma000 <tanujasharma08080@gmail.com>
2025-07-06 05:59:38 -0700

    Update db_connect2.py

--- db_connect2.py
+++ db_connect2.py
@@ -1,14 +1,16 @@
 import mysql.connector
+import os 
+
 def get_connection():
     try:
-        conn=mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="xyz",
-        database="library_db"
+        conn = mysql.connector.connect(
+            host="localhost",
+            user="root",
+            password=os.getenv("DB_PASSWORD"), 
+            database="library_db"
         )
         return conn
-    except mysql.connector.error as er:
-        print("Error while connecting to database:", er)
+    except mysql.connector.Error as er: 
+        print(f"Database connection failed: {er}")
         return None
 
